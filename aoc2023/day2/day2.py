88b2206393ce2ea749853af4bf5fadc93cddb04b
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2023-12-02 17:09:18 +0100

    Day2 initial

---
+++ aoc2023/day2/day2.py
@@ -0,0 +1,12 @@
+from aoc2023.util import Day
+
+class Day2(Day):
+    def __init__(self):
+        super('part1.test')
+        
+    def solve_part1(self):
+        print(self.data) 
+        
+        
+day = Day2()
+day.run()  
